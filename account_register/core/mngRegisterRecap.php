fcdc7b290ae3b0d658baec496288683af99ccf8a
davide <davide.masera@xstream-labs.com>
2023-03-24 17:08:34 +0100

    adaptep plugins to new calss initialization system

--- account_register/core/mngRegisterRecap.php
+++ account_register/core/mngRegisterRecap.php
@@ -26,6 +26,22 @@ $db = $database->getConnection();
 
 include "../inc/class_initialize.php";
 
+$module = $plugin->showAll('id');
+foreach($module as $row){
+    $plugin->pluginname = $row['pluginname'] ;
+        if($plugin->itemExists('pluginname') && $plugin->isActive()==1){
+            $scan = scandir("plugins/".$row['pluginname']."/class");
+            $exclude = array('..', '.','.gitkeep');
+            foreach($scan as $file){
+            if (!in_array($file,$exclude)) {
+                $item = pathinfo($file);
+                include "class/plugin/".$item['basename']."";
+            }
+        }
+    }
+}
+
+
 $setting->name="lang" ;
 $stmt = $setting->showByName();
 $lang = $stmt['value'];
