4eff8123bb86ea90d5908ee32f793674690d3a8b
davide <davide.masera@xstream-labs.com>
2024-06-13 14:15:17 +0200

    set new base structure for menu links

--- base_module/config.php
+++ base_module/config.php
@@ -17,26 +17,22 @@ $query_create_table = "CREATE TABLE IF NOT EXISTS " . $prefix . "table_name
 
 // the data of the parent item of the menu
 
-$parent_table = [[
+$menu_link = [[
       'link' => 'base_module',
       'label' => 'Base module',
-      'icon' => 'icon-name'
-]];
-
-// the data of the child item of the menu
-// they will automatically get the previous parent item id
-
-$child_table = [
-      [
-            'link' => 'allBaseModule',
-            'label' => 'All base module',
-            'icon' => 'icon-name'
-      ],
-      [
-            'link' => 'addBaseModule',
-            'label' => 'Add a new base module',
-            'icon' => 'people-fill'
+      'icon' => 'icon-name',
+      'child' => [
+            [
+                  'link' => 'allBaseModule',
+                  'label' => 'All base module',
+                  'icon' => 'icon-name'
+            ],
+            [
+                  'link' => 'addBaseModule',
+                  'label' => 'Add a new base module',
+                  'icon' => 'people-fill'
+            ]
       ]
-];
+]];
 
 $query_drop_table = "DROP TABLE  " . $prefix . "table_name, " . $prefix . "second_table_name, " . $prefix . "luna_users ";
